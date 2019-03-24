bed96d6ccc21c6908c2f1763d9fd5159ffccd8d8
Mahendra Bhama <maahibhama@gmail.com>
2019-03-24 16:17:04 +0530

    Added i18n to project

---
+++ src/localization/locales/en.js
@@ -0,0 +1,9 @@
+export default {  
+    welcome01: "Explore the world",
+    welcome02: "Travel with people. Make new friends.",
+    welcome03: "SIGN IN",
+    welcome04: "SIGN UP",
+    signInO1: "Sign In",
+    signUp01: "Sign Up"
+};
+  
