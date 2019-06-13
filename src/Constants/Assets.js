7c37e46c4e7044c0fab495a003ef0eaaaa79a387
Mahendra Bhama <maahibhama@gmail.com>
2019-06-13 22:16:01 +0530

    Update Assets.js

--- src/Constants/Assets.js
+++ src/Constants/Assets.js
@@ -1,7 +1,13 @@
 export const Images = {
-    splash: require("../Assets/Images/Splash/Splash.png"),
+  splash: require('../Assets/Images/Splash/Splash.png')
 }
 
 export const Icons = {
-    back: require("../Assets/Icons/back_icon/back_icon.png")
+  back: require('../Assets/Icons/back_icon/back_icon.png'),
+  travel: require('../Assets/Icons/travel_icon/travel_icon.png'),
+  flight: require('../Assets/Icons/flight_icon/flight_icon.png'),
+  hotels: require('../Assets/Icons/hotels_icon/hotels_icon.png'),
+  chartbot: require('../Assets/Icons/chartbot_icon/chartbot_icon.png'),
+  profile: require('../Assets/Icons/profile_icon/profile_icon.png'),
+  profileEdit: require('../Assets/Icons/profile_edit_icon/profile_edit_icon.png')
 }
