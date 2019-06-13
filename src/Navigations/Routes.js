e674031f346f4a2e1cfa97f823d97a52b29cdf8c
Mahendra Bhama <maahibhama@gmail.com>
2019-06-13 22:19:48 +0530

    Update Routes.js

--- src/Navigations/Routes.js
+++ src/Navigations/Routes.js
@@ -1,8 +1,19 @@
 export default {
-    AuthNavigator: "AuthNavigator",
-    TabNavigator: "TabNavigator",
-    AuthLoadingView: "AuthLoadingView",
-    WelcomeView: "WelcomeView",
-    LoginView: "LoginView",
-    SignUpView: "SignUpView"
+  AuthNavigator: 'AuthNavigator',
+  TabNavigator: 'TabNavigator',
+  TravelNavigator: 'TravelNavigator',
+  FlightsNavigator: 'FlightsNavigator',
+  ChartbotNavigator: 'ChartbotNavigator',
+  HotelsNavigator: 'HotelsNavigator',
+  ProfileNavigator: 'ProfileNavigator',
+  AuthLoadingView: 'AuthLoadingView',
+  WelcomeView: 'WelcomeView',
+  LoginView: 'LoginView',
+  SignUpView: 'SignUpView',
+  ForgotPasswordView: 'ForgotPasswordView',
+  TravelFeedView: 'TravelFeedView',
+  FlightsFeedView: 'FlightsFeedView',
+  ChartbotView: 'ChartbotView',
+  HotelsFeedView: 'HotelsFeedView',
+  ProfileView: 'ProfileView'
 }
