9636bdae1c886c27f60886345a210a9b83008903
Mahendra Bhama <maahibhama@gmail.com>
2019-06-13 22:16:24 +0530

    Update en.js

--- src/localization/locales/en.js
+++ src/localization/locales/en.js
@@ -1,9 +1,19 @@
-export default {  
-    welcome01: "Explore the world",
-    welcome02: "Travel with people. Make new friends.",
-    welcome03: "SIGN IN",
-    welcome04: "SIGN UP",
-    signInO1: "Sign In",
-    signUp01: "Sign Up"
-};
-  
+export default {
+  welcome01: 'Explore the world',
+  welcome02: 'Travel with people. Make new friends.',
+  welcome03: 'SIGN IN',
+  welcome04: 'SIGN UP',
+  signIn01: 'Sign In',
+  signIn02: 'Email Address',
+  signIn03: 'Password',
+  signIn04: 'Forgot your password?',
+  signUp01: 'Create New Account',
+  signUp02: 'Name',
+  signUp03: 'Already have an account?',
+  forgotPassword01: 'Reset your password',
+  profile01: 'Profile',
+  flights01: 'Flight',
+  chartbot01: 'Chartbot',
+  hotels01: 'Hotels',
+  travel01: 'Travel'
+}
