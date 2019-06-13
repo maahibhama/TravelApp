3926cd6fd5902ed20d75b1284a78bc921b496281
Mahendra Bhama <maahibhama@gmail.com>
2019-06-13 22:19:38 +0530

    Update TabNavigator.js

--- src/Navigations/TabNavigator.js
+++ src/Navigations/TabNavigator.js
@@ -1,19 +1,34 @@
-import { createStackNavigator } from "react-navigation";
-import WelcomeView from "../Views/GetStarted/WelcomeView";
-import LoginView from "../Views/GetStarted/LoginView";
-import SignUpView from "../Views/GetStarted/SignUpView";
-import Routes from "./Routes";
+import { createBottomTabNavigator } from 'react-navigation'
+import { Image } from 'react-native'
 
-export const TabNavigator = createStackNavigator(
-    {
-        [Routes.Welcome]: { screen: WelcomeView },
-        [Routes.Login]: { screen: LoginView },
-        [Routes.SignUp]: { screen: SignUpView }
-    },
-    {
-        navigationOptions: ({ navigation }) => ({
-            gesturesEnabled: false
-        }),
-        initialRouteName: Routes.Welcome,
-    }
-);
+import { TravelNavigator } from './TravelNavigator'
+import { FlightsNavigator } from './FlightsNavigator'
+import { ChartbotNavigator } from './ChartbotNavigator'
+import { HotelsNavigator } from './HotelsNavigator'
+import { ProfileNavigator } from './ProfileNavigator'
+
+import Routes from './Routes'
+import { Icons } from '../Constants/Assets'
+import { Color } from '../Constants/Colors'
+
+export const TabNavigator = createBottomTabNavigator(
+  {
+    [Routes.TravelNavigator]: { screen: TravelNavigator },
+    [Routes.FlightsNavigator]: { screen: FlightsNavigator },
+    [Routes.ChartbotNavigator]: { screen: ChartbotNavigator },
+    [Routes.HotelsNavigator]: { screen: HotelsNavigator },
+    [Routes.ProfileNavigator]: { screen: ProfileNavigator }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarOptions: {
+        activeTintColor: Color.themeDark,
+        inactiveTintColor: Color.defaultTheme
+      }
+    }),
+    navigationOptions: ({ navigation }) => ({
+      gesturesEnabled: false
+    }),
+    initialRouteName: Routes.TravelNavigator
+  }
+)
