95dc2be070129aca387a4e0e511e9fe24b06741d
Mahendra Bhama <maahibhama@gmail.com>
2019-09-08 01:34:17 +0530

    Create Constants.js

---
+++ src/Constants/Constants.js
@@ -0,0 +1,44 @@
+export const Places = [
+  {
+    id: '1',
+    image: PlaceUrl,
+    name: 'Vienna'
+  },
+  {
+    id: '2',
+    image: PlaceUrl,
+    name: 'Vienna'
+  },
+  {
+    id: '3',
+    image: PlaceUrl,
+    name: 'Vienna'
+  },
+  {
+    id: '4',
+    image: PlaceUrl,
+    name: 'Vienna'
+  }
+]
+
+export const Countries = [
+  {
+    id: '1',
+    name: 'Asia'
+  },
+  {
+    id: '2',
+    name: 'Europe'
+  },
+  {
+    id: '3',
+    name: 'East'
+  },
+  {
+    id: '4',
+    name: 'South Africa'
+  }
+]
+
+export const PlaceUrl =
+  'https://www.wien.info/media/images/40367-graben-einkaufen-shopping-altstadt-einkaufsstrassen-3to2.jpeg/image_teaser-lead'
