dbc59b6eafda003e2ef9439d47e7462f98789e6c
Mahendra Bhama <maahibhama@gmail.com>
2019-09-08 01:35:13 +0530

    Update Travel feed View

--- src/Views/Travel/TravelFeedView/index.js
+++ src/Views/Travel/TravelFeedView/index.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, View } from 'react-native'
 import PropTypes from 'prop-types'
 
+import I18n from '../../../localization/i18n'
+import { Icons } from '../../../Constants/Assets'
+import TitleNavigationHeader from '../../../Components/navigation-header/TitleNavigationHeader'
+import ManageKeyboardScrollView from '../../../Constants/ManageKeyboardScrollView'
+import CountryCollcetionView from '../../../Components/CountryCollcetionView'
+
 import styles from './styles'
+import { Countries } from '../../../Constants/Constants'
 
 class TravelFeedView extends Component {
+  static navigationOptions = {
+    header: null
+  }
+
   static defaultProps = {}
 
   static propTypes = {}
 
   render () {
-    return <SafeAreaView style={styles.container} />
+    return (
+      <SafeAreaView style={styles.container}>
+        <TitleNavigationHeader
+          navigation={this.props.navigation}
+          title={I18n.t('travel02')}
+          showRightButton
+          rightButtonIcon={Icons.search}
+          onClickRightButton={this.onClickRightButton}
+        />
+        <ManageKeyboardScrollView
+          keyboardShouldPersistTaps={'always'}
+          contentContainerStyle={styles.keyboardAvoidView}
+        >
+          {this.renderMiddleView()}
+        </ManageKeyboardScrollView>
+      </SafeAreaView>
+    )
+  }
+
+  renderMiddleView () {
+    return (
+      <View style={styles.middleView}>
+        {this.renderCountryCollectionView()}
+      </View>
+    )
+  }
+
+  renderCountryCollectionView () {
+    return <CountryCollcetionView data={Countries} />
   }
 }
 export default TravelFeedView
