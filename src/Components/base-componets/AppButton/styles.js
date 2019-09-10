5960ac36b1100c5391597001a93e379910156c65
Mahendra Bhama <maahibhama@gmail.com>
2019-09-10 20:32:16 +0530

    Add app button shadow

--- src/Components/base-componets/AppButton/styles.js
+++ src/Components/base-componets/AppButton/styles.js
@@ -3,16 +3,19 @@ import { AppFont } from '../../../Constants/Fonts'
 import { Color } from '../../../Constants/Colors'
 
 export default StyleSheet.create({
-    viewStyle:{
-        height: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 5
-    },
-    textStyle:{
-        fontSize: AppFont.titleLargeBold.size,
-        fontFamily: AppFont.titleLargeBold.name,
-        fontWeight: AppFont.titleLargeBold.weight,
-        color: Color.themeDark
-    }
+  viewStyle: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    shadowColor: Color.shadow,
+    shadowOpacity: 0.1,
+    elevation: 3
+  },
+  textStyle: {
+    fontSize: AppFont.titleLargeBold.size,
+    fontFamily: AppFont.titleLargeBold.name,
+    fontWeight: AppFont.titleLargeBold.weight,
+    color: Color.themeDark
+  }
 })
