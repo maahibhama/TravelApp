9c380cdc3a3c88b45743cbfe66237416c06a09f0
Mahendra Bhama <maahibhama@gmail.com>
2019-09-14 00:54:20 +0530

    Added Dot View

--- src/Components/FavoritePlacesView/index.js
+++ src/Components/FavoritePlacesView/index.js
@@ -1,52 +1,76 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { View, FlatList } from "react-native";
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { View, FlatList } from 'react-native'
 
-import PlaceView from "./PlaceView";
-import styles from "./styles";
-
+import PlaceView from './PlaceView'
+import styles from './styles'
+import DotView from './DotView'
 
 export default class FavoritePlacesView extends PureComponent {
-    static propTypes = {
-        data: PropTypes.array,
-        navigation: PropTypes.object.isRequired
-    };
+  static propTypes = {
+    data: PropTypes.array,
+    navigation: PropTypes.object.isRequired
+  }
 
-    static defaultProps = {
-        data: [],
+  static defaultProps = {
+    data: []
+  }
+
+  state = {
+    viewedIndex: 0
+  }
+
+  renderItem = this.renderItem.bind(this)
+  handleViewableItemsChanged = this.handleViewableItemsChanged.bind(this)
+
+  onClickSeeAll () {
+    // this.props.navigation.navigate(Routes.Featured)
+  }
+
+  onTouchCategory (item) {
+    // this.props.navigation.navigate(Routes.Featured)
+  }
+
+  handleViewableItemsChanged (info) {
+    if (info.viewableItems[0] != null) {
+      let indexValue = info.viewableItems[0].index
+      this.setState({ viewedIndex: indexValue })
     }
+  }
 
-    renderItem = this.renderItem.bind(this)
+  render () {
+    const { data } = this.props
 
-    onClickSeeAll() {
-       // this.props.navigation.navigate(Routes.Featured)
-    }
+    return (
+      <View style={styles.containerView}>
+        <FlatList
+          extraData={this.state}
+          data={data}
+          horizontal
+          pagingEnabled
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => item.id}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.tableView}
+          onViewableItemsChanged={this.handleViewableItemsChanged}
+        />
+        {this.renderDotView({ data: data })}
+      </View>
+    )
+  }
 
-    onTouchCategory(item) {
-        //this.props.navigation.navigate(Routes.Featured)
-    }
+  renderItem ({ item }) {
+    return (
+      <PlaceView
+        info={item}
+        onTouch={() => {
+          this.onTouchCategory(item)
+        }}
+      />
+    )
+  }
 
-    render() {
-        return (
-            <View style={styles.containerView}>
-                <FlatList
-                    extraData={this.state}
-                    data={this.props.data}
-                    horizontal={true}
-                    pagingEnabled={true}
-                    renderItem={this.renderItem}
-                    keyExtractor={(item, index) => item.id}
-                    showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={styles.tableView}
-                />
-            </View>
-        )
-    }
-
-    renderItem({ item }) {
-        return (
-            <PlaceView info={item} onTouch={() => { this.onTouchCategory(item) }} />
-        )
-    }
-
+  renderDotView ({ data }) {
+    return <DotView data={data} selectedItemIndex={this.state.viewedIndex} />
+  }
 }
